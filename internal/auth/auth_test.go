87433d747c49e1059d2c3e7537e92c237eb4fcf7
Daniel Dvořák <dvorad35@fit.cvut.cz>
2025-07-07 14:15:32 +0200

    add tests for auth + failing test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	h := make(map[string][]string, 0)
+
+	h["akldjf"] = []string{"kdsaf", "akdslf"}
+	_, err := GetAPIKey(h)
+	if err == nil {
+		t.Fatal("Nonsense header should error")
+	}
+
+	h["Authorization"] = []string{"ApiKey topsecreatekeya1893841", " ths is some garbaseg"}
+	key, err := GetAPIKey(h)
+	if err != nil {
+		t.Fatal("Should find the key")
+	}
+	if !reflect.DeepEqual("topsecreatekeya1893841", key) {
+		t.Fatal("Key does not match")
+	}
+}
+
+func TestFailing(t *testing.T) {
+	t.Fatal("This fails always")
+}
